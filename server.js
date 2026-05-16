ed63100d7b3eb3b40f77816c6e407dda3ef8b242
ssttraning <ssttraning@gmail.com>
2026-05-16 13:33:45 +0530

    Added home route

--- server.js
+++ server.js
@@ -16,3 +16,6 @@ app.use('/insertstudent', insertstudent)
 app.listen(port, () => {
     console.log("Server is running on port 3000")
 })
+app.get('/', (req, res) => {
+    res.send("Backend is running successfully")
+})
